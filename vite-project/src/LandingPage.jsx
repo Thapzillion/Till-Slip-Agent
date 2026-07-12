4d9497094df6cb2475e8a49c534f8d816676d799
Thapzillion <messengerthapelo@gmail.com>
2026-07-12 16:15:27 +0200

    Image Web app fix

--- vite-project/src/LandingPage.jsx
+++ vite-project/src/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import dashboardImg from './dashboard-preview.png';
-import receiptviewImg from './receipt-preview.png';
+import dashboardImg from '.dashboard-preview.png';
+import receiptviewImg from '.receipt-preview.png';
 
 export default function LandingPage() {
   // Handles the smooth anchor link navigation behavior natively in React
